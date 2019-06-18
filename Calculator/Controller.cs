f8964bdd49f35d2f1e608efa3d3f818c814dbcbc
razvan1802 <40825691+razvan1802@users.noreply.github.com>
2019-06-18 19:16:08 +0300

    Add files via upload

---
+++ Calculator/Controller.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	
+	class Controller
+	{
+		/// <summary>
+		/// imprima un meniu
+		/// </summary>
+		public void PrintMenu()
+		{
+			Console.WriteLine("Selectati functia de rulat:");
+
+			Console.WriteLine("1.Plus1");
+			Console.WriteLine("2.Plus2");
+			Console.WriteLine("3.Plus3");
+			Console.WriteLine("4.Plus4");
+			Console.WriteLine("5.Plus5");
+			Console.WriteLine("Esc pentru a iesi");
+		}
+
+        /// <summary>
+        /// efectueaza lucrul principal cu utilizatorul
+        /// </summary>
+        /// <returns>returneaza false pentru a termina programul</returns>
+        public bool Menu()
+		{
+			PrintMenu();
+			ConsoleKey ch = Console.ReadKey(true).Key;
+			if (ch == ConsoleKey.Escape)
+				return false;
+
+			Console.Clear();
+
+			try
+			{
+				Console.WriteLine("Rezultatul: {0}\n\n", Switcher(ch));
+			}
+			catch (ArgumentException exc)
+			{
+				if (exc.Message == "Intrare necorespunzatoare")
+					Console.WriteLine("Metoda gresita de a introduce argumente\n\n");
+				else if (exc.Message == "Scadere")
+					Console.WriteLine("Nu puteti introduce numere negative in aceasta functie\n\n");
+				else if (exc.Message == "Niciun element de meniu")
+					Console.WriteLine("Puteti alege prin apasarea tastelor numerice\n\n");
+			}
+			return true;
+		}
+
+        /// <summary>
+        /// organizarea instructiunilor de comutare, functiile de apel Plus
+        /// </summary>
+        /// <param name="ch">cheie apasata de utilizator</param>
+        /// <returns>returneaza rezultatul functiei plus</returns>
+        public double Switcher(ConsoleKey ch)
+		{
+			Plus a = new Plus();
+
+			switch (ch)
+			{
+				case ConsoleKey.D1:
+				case ConsoleKey.NumPad1:
+					return a.Plus1(ReadArguments());
+				case ConsoleKey.D2:
+				case ConsoleKey.NumPad2:
+					return a.Plus2(ReadArguments());
+				case ConsoleKey.D3:
+				case ConsoleKey.NumPad3:
+					return a.Plus3(ReadArguments());
+				case ConsoleKey.D4:
+				case ConsoleKey.NumPad4:
+					return a.Plus4(ReadArguments());
+				case ConsoleKey.D5:
+				case ConsoleKey.NumPad5:
+					return a.Plus5(ReadArguments());
+				default: throw new ArgumentException("niciun element de meniu");
+			}
+		}
+
+        /// <summary>
+        /// interogari si citeste sirul de argumente pentru functia Plus
+        /// </summary>
+        /// <returns></returns>
+        public string ReadArguments()
+		{
+			Console.WriteLine("Introduceti un sir de argumente: ");
+			return Console.ReadLine();
+		}
+
+	}
+}
---
+++ Calculator/Plus.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /// <summary>
+    /// implementeaza functia de adaugare
+    /// </summary>
+    public class Plus
+	{
+        /// <summary>
+        /// expresie regulata pentru gasirea numerelor cu semne posibile + si - (de exemplu, 5, +6, -9)
+        /// </summary>
+        Regex number1 = new Regex(@"[+-]?\d+(\.\d)?\d*");
+
+        /// <summary>
+        /// expresia regulata este similara cu cea anterioara, dar fara semn -
+		/// </summary>
+		Regex number2 = new Regex(@"\+?\d+(\.\d)?\d*");
+
+        /// <summary>
+        /// rezultat suplimentar
+		/// </summary>
+		double res = 0;
+
+        /// <summary>
+        /// argument suplimentar
+        /// </summary>
+        double arg;
+        /// <summary>
+        /// expresie regulata pentru a afla daca un sir dat poate fi procesat
+        /// </summary>
+        Regex reg;
+
+        /// <summary>
+        /// adauga cele doua numere listate în linia de intrare separate prin virgule
+        /// </summary>
+        public double Plus1(string argStr)
+		{
+			reg = new Regex(@"^[+-]?\d+(\.\d)?\d*,[+-]?\d+(\.\d)?\d*$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("intrare necorespunzatoare");
+			
+			foreach (Match m in number1.Matches(argStr))
+			{
+				if (Double.TryParse(m.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out arg))
+					res += arg;
+			}
+
+			return res;
+		}
+
+        /// <summary>
+        /// imbunatatire Plus1
+        /// spre deosebire de Plus1, ia un numar arbitrar de argumente
+        /// </summary>
+        public double Plus2(string argStr)
+		{
+			reg = new Regex(@"^[+-]?\d+(\.\d)?\d*(,[+-]?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("intrare necorespunzatoare");
+
+			foreach (Match m in number1.Matches(argStr))
+			{
+				if (Double.TryParse(m.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out arg))
+					res += arg;
+			}
+
+			return res;
+		}
+
+        /// <summary>
+        /// imbunatatire Plus2
+        /// spre deosebire de Plus2, intelege mai multe delimitatoare
+        /// </summary>
+        public double Plus3(string argStr)
+		{
+			reg = new Regex(@"^[+-]?\d+(\.\d)?\d*([	, ~!@#$%^&*()_+-][+-]?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("intrare necorespunzatoare");
+
+			foreach (Match m in number1.Matches(argStr))
+			{
+				if (Double.TryParse(m.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out arg))
+					res += arg;
+			}
+
+			return res;
+		}
+
+        /// <summary>
+        /// imbunatatire Plus3
+        /// spre deosebire de Plus3, accepta la numere negative
+        /// </summary>
+        public double Plus4(string argStr)
+		{
+			reg = new Regex(@"^[+-]?\d+(\.\d)?\d*([	, ~!@#$%^&*()_+-][+-]?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("intrare necorespunzatoare");
+
+			reg = new Regex(@"^\+?\d+(\.\d)?\d*([	, ~!@#$%^&*()_+-]\+?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("scadere");
+
+			foreach (Match m in number2.Matches(argStr))
+			{
+				if (Double.TryParse(m.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out arg))
+					res += arg;
+			}
+
+			return res;
+		}
+
+        /// <summary
+        /// imbunatatire Plus4
+        /// spre deosebire de Plus4, puteti utiliza 1 pana la 3 delimitatori
+        /// </summary>
+        public double Plus5(string argStr)
+		{
+			reg = new Regex(@"^[+-]?\d+(\.\d)?\d*([	, ~!@#$%^&*()_+-]{1,3}[+-]?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("intrare necorespunzatoare");
+
+			reg = new Regex(@"^\+?\d+(\.\d)?\d*([	, ~!@#$%^&*()_+-]{1,3}\+?\d+(\.\d)?\d*)+$");
+
+			if (!reg.IsMatch(argStr))
+				throw new ArgumentException("scadere");
+
+			foreach (Match m in number2.Matches(argStr))
+			{
+				if (Double.TryParse(m.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out arg))
+					res += arg;
+			}
+
+			return res;
+		}
+	}
+}
